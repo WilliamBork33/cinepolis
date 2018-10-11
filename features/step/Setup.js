e66ba81fb31eecc975cb87109c79fb49086cd3b4
WilliamBork33 <williambork33@gmail.com>
2018-10-11 16:23:10 -0500

    Initial Commit

---
+++ features/step/Setup.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+    
+    this.Given(/^we're in Cinepolis$/, function () {
+        // Write code here that turns the phrase above into concrete actions
+
+        browser.url('http://www.cinepolis.com/');
+
+      });
+
+}
+
